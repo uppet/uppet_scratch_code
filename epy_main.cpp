c341d97c74e4aca00ce9517ba48f0f0d30b1731e
uppet <collger@sina.com>
2009-07-30 21:46:52 +0800

    实现TD 的lexer

--- epy_main.cpp
+++ epy_main.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include<vector>
 #include<iostream>
+#include<sstream>
 #include<fstream>
 #include<cctype>
 namespace epy{
@@ -9,12 +10,11 @@ namespace epy{
     using std::string;
     using std::ifstream;
     using std::isalpha;
+    using std::isalnum;
+    using std::stringstream;
 
     class token;
     struct token_imp;
-    enum token_type;
-    vector<token> get_tokens(const string &str);
-    token         make_tok(token_type, const string &string_val, double double_val);
 
     enum token_type {
         OPERATOR,
@@ -22,6 +22,9 @@ namespace epy{
         STRING,
         NUMBER
     };
+    
+    vector<token> get_tokens(const string &str);
+    token         make_tok(token_type, const string &string_val, double double_val);
 
     struct token_imp {
         string string_val;
@@ -29,6 +32,14 @@ namespace epy{
         int    int_val;
         int    pos_from;
         int    pos_to;
+        void error(const string &err_msg) {
+            std::cout<<err_msg<<to_string();
+        }
+        string to_string() {
+            stringstream ss;
+            ss<<"\ns: "<<string_val<<"\nd: "<<double_val<<std::endl;
+            return ss.str();
+        };
     };
 
     /**
@@ -39,7 +50,8 @@ namespace epy{
         int       * cnt;
         void dec_internal() {
             if (--(*cnt) < 1) {
-                std::cout<<"die"<<ob<<ob->string_val<<std::endl;
+                //std::cout<<"die"<<ob<<ob->to_string()<<std::endl;
+                std::cout<<ob->string_val<<std::endl;
                 delete cnt;
                 delete ob;
             }
@@ -97,6 +109,9 @@ namespace epy{
         vector<token> tokens;
         size_t i = 0;
         int from = 0;
+
+        string prefix = "<>+-&";
+        string suffix = "=>&:";
         
         char c = charat(str, i);
         while (c) {
@@ -119,7 +134,7 @@ namespace epy{
                     break;
                 }
                 tokens.push_back(make_token(NAME, str_val, 0));
-            } else if (c >= '0' || c <= '9' ) {
+            } else if (c >= '0' && c <= '9' ) {
                 str_val += c;
                 i++;
                 for(;;) {
@@ -141,29 +156,119 @@ namespace epy{
                         str_val += c;
                     }
                 }
-                if (c === 'e' || c === 'E') {
+                if (c == 'e' || c == 'E') {
                     i += 1;
                     str_val += c;
                     c = charat(str, i);
-                    if (c === '-' || c === '+') {
+                    if (c == '-' || c == '+') {
                         i += 1;
                         str_val += c;
                         c = charat(str, i);
                     }
                     if (c < '0' || c > '9') {
-                        make('number', str_val, strtod(str_val, 0)).error("Bad exponent");
+                        make_token(NUMBER, str_val, strtod(str_val.c_str(), 0))->error("Bad exponent");
                     }
                     do {
                         i += 1;
                         str_val += c;
-                        c = this.charAt(i);
+                        c = charat(str, i);
                     } while (c >= '0' && c <= '9');
                 }
-            }
-            else {
+                if (isalpha(c)) {
+                    str_val += c;
+                    i += 1;
+                    make_token(NUMBER, str_val, strtod(str_val.c_str(), 0))->error("Bad number");
+                }
+
+                tokens.push_back(make_token(NUMBER, str_val, strtod(str_val.c_str(), 0)));
+            } else if (c == '\'' || c == '"') {
+                str_val = "";
+                char q = c;
                 i++;
+                for(;;) {
+                    c = charat(str, i);
+                    if (c < ' ') {
+                        make_token(STRING, str_val, 0)->error( c == '\n' || c == '\r' || c == 0 ?
+                                                               "Unterminated string." :
+                                                               "Control character in string."); //TODO append make('',str_val)
+                    }
+
+                    if (c == q) {
+                        break;
+                    }
+
+                    if (c == '\\') {
+                        i++;
+                        if (i >= str.size()) {
+                            make_token(STRING, str_val, 0)->error("Unterminated string.");
+                        }
+                        c = charat(str, i);
+                        switch(c) {
+                        case 'b':
+                            c = '\b';
+                            break;
+                        case 'f':
+                            c = '\f';
+                            break;
+                        case 'n':
+                            c = '\n';
+                            break;
+                        case 'r':
+                            c = '\r';
+                            break;
+                        case 't':
+                            c = '\t';
+                            break;
+                        case 'u':
+                            if (i >= str.size()) {
+                                make_token(STRING, str_val, 0)->error("Unterminated string.");
+                            }
+                            c = atoi(str.substr(i + 1, 4).c_str());
+//                             if (!isFinite(c) || c < 0) {
+//                                 make('string', str).error("Unterminated string");
+//                             }
+                            i += 4;
+                            break;                            
+                        }
+                    }
+                    str_val += c;
+                    i++;
+                }
+                i++;
+                tokens.push_back(make_token(STRING, str_val, 0));
+                c = charat(str, i);
+            } else if (c == '/' && charat(str, i + 1) == '/') {
+                i++;
+                for (;;) {
+                    c = charat(str, i);
+                    if (c == '\n' || c == '\r' || c == 0) {
+                        break;
+                    }
+                    i += 1;
+                }
+            } else if (prefix.find(c) != string::npos) {
+                str_val = c;
+                i++;
+                while (i < str.size()) {
+                    c = charat(str, i);
+                    if (suffix.find(c) == string::npos) {
+                        break;
+                    }
+                    str_val += c;
+                    i++;
+                }
+                tokens.push_back(make_token(OPERATOR, str_val, 0));
+
+            } else {
+                i++;
+                str_val += c;
+                tokens.push_back(make_token(OPERATOR, str_val, 0));
                 c = charat(str, i);
             }
+//             else {  /// Final un reached;
+//                 i++;
+//                 c = charat(str, i);
+//             }
         }
     
         return tokens;
@@ -197,11 +302,11 @@ int main(int argc, char **argv)
 {
     using namespace std;
     vector<epy::token> toks;
-    string file = "h:\\check\\epvm\\fe\\epy_main.cpp";
+    string file = "tokens.js";
     if (argc > 2)
         file = argv[1];
     toks = epy::get_tokens(epy::whole_file(file));
-    cout<<toks.size()<<endl;
+    cout<<toks.size()<<" tokens in total"<<endl;
 
 
     return 0;
