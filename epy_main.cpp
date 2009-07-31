d0a6ccbb3bc6b9c9f1c6b2dbc81229314f0ae28b
uppet <collger@sina.com>
2009-07-31 17:40:21 +0800

    初步进入动态语义移植

--- epy_main.cpp
+++ epy_main.cpp
@@ -1,3 +1,4 @@
+#include"prec.h"
 
 #include<string>
 #include<vector>
@@ -5,6 +6,7 @@
 #include<sstream>
 #include<fstream>
 #include<cctype>
+#include<map>
 namespace epy{
     using std::vector;
     using std::string;
@@ -12,13 +14,76 @@ namespace epy{
     using std::isalpha;
     using std::isalnum;
     using std::stringstream;
+    using std::map;
 
-    class token;
+    /**
+       带引用管理handle对象
+     */
+    template<typename T, bool hold = true>
+    class handle_rc {
+        friend class handle_rc<T, !hold>;
+        T * ob;
+        int       * cnt;
+        void dec_internal() {
+            if (--(*cnt) < 1) {
+                //std::cout<<"die"<<ob<<ob->to_string()<<std::endl;
+                //std::cout<<ob->to_string();
+                delete cnt;
+                delete ob;
+            }
+        }
+        void inc_internal() {
+            *cnt += 1;
+        }
+    public:
+        handle_rc(const handle_rc &other) {
+            ob = other.ob;
+            cnt = other.cnt;
+            inc_internal();
+        }
+        handle_rc() {
+            ob = NULL;
+            if (hold)
+                ob  = new T;
+            cnt = new int(1);
+        }
+        T * operator->() {
+            return ob;
+        }
+        T * it() {
+            return ob;
+        }
+        template<typename T2>
+        handle_rc& operator=(const T2  &other) {
+            dec_internal();
+            ob = other.ob;
+            cnt = other.cnt;
+            inc_internal();
+            return *this;
+        }
+        handle_rc& operator=(const handle_rc  &other) {
+            dec_internal();
+            ob = other.ob;
+            cnt = other.cnt;
+            inc_internal();
+            return *this;
+        }
+        ~handle_rc() {
+            dec_internal();
+        }
+    };
+    
+    template<typename T, bool>
+    class handle_rc;
+
     struct token_imp;
-    class tdparser;
     struct tdparser_imp;
-
     struct scope_imp;
+
+    typedef handle_rc<token_imp> token;
+    typedef handle_rc<tdparser_imp> tdparser;
+    typedef handle_rc<scope_imp> scope;
+    typedef handle_rc<scope_imp, false> scopeh;
     
     enum token_type {
         OPERATOR,
@@ -26,131 +91,158 @@ namespace epy{
         STRING,
         NUMBER
     };
-    
+    enum arity_type {
+        ARITY_NAME,
+        ARITY_STATEMENT,
+        ARITY_UNARY,
+        ARITY_BINARY,
+        ARITY_LITERAL,
+        ARITY_FUNCTION,
+        ARITY_TERNARY,
+        ARITY_THIS
+    };
+
+    struct nud_func{
+        nud_func(){
+            m_nud = NULL;
+        }
+        nud_func(token (*nud)(token , tdparser_imp *)) {
+            m_nud = nud;
+        }
+        token operator()(token tok, tdparser_imp* prsr) {
+            return do_call(tok, prsr);
+        }
+        virtual token do_call(token tok, tdparser_imp* prsr) {
+            if (m_nud)
+                return m_nud(tok, prsr);
+        }
+        token (*m_nud)(token , tdparser_imp *);
+    };
+
+    map<string, token> symbol_table;
+
     vector<token> get_tokens(const string &str);
     token         make_tok(token_type, const string &string_val, double double_val);
 
     struct token_imp {
-        string string_val;
-        double double_val;
-        int    int_val;
-        int    pos_from;
-        int    pos_to;
+        token_type type;
+        string     string_val;
+        double     double_val;
+        int        int_val;
+        string     id;
+        bool       reserved;
+        bool       isassign;
+        token      (*nud)(token tok, tdparser_imp *prsr);
+        token      (*led)(token tok, token left, tdparser_imp *prsr);
+        token      (*std)(token tok, tdparser_imp *prsr);
+        int        lbp;
+        int        rbp;
+        arity_type arity;
+        scope      at_scope;
+        int        pos_from;
+        int        pos_to;
+        vector<token> subs;
         void error(const string &err_msg) {
             std::cout<<err_msg<<to_string();
+            exit(1);
         }
         string to_string() {
             stringstream ss;
-            ss<<"\ns: "<<string_val<<"\nd: "<<double_val<<std::endl;
+            //ss<<"\ns: "<<string_val<<"\nd: "<<double_val<<std::endl;
+            ss<<string_val<<std::endl;
             return ss.str();
         };
     };
 
-    /**
-       带引用管理token对象
-     */
-    class token {
-        token_imp * ob;
-        int       * cnt;
-        void dec_internal() {
-            if (--(*cnt) < 1) {
-                //std::cout<<"die"<<ob<<ob->to_string()<<std::endl;
-                std::cout<<ob->string_val<<std::endl;
-                delete cnt;
-                delete ob;
-            }
-        }
-        void inc_internal() {
-            *cnt += 1;
-        }
-    public:
-        token(const token &other) {
-            ob = other.ob;
-            cnt = other.cnt;
-            inc_internal();
-        }
-        token() {
-            ob  = new token_imp;
-            cnt = new int(1);
-        }
-        token_imp * operator->() {
-            return ob;
-        }
-        token& operator=(const token &other) {
-            dec_internal();
-            ob = other.ob;
-            cnt = other.cnt;
-            inc_internal();
-            return *this;
-        }
-        ~token() {
-            dec_internal();
-        }
-    };
-
+    static scope s_cur_scope;
     struct scope_imp {
-        void define (token tok) {
-            
-        }
+        map<string, token> m_def;
+        scopeh   parent;
+        
+        token define (token tok);
+        token find (string name);
+        void pop ();
+        void reserve (token tok);
+        
+        string to_string() {
+            stringstream ss;
+            return ss.str();
+        };
     };
 
     struct tdparser_imp {
-        vector<token> (*tokens_gen)(const string &);
+        vector<token> (*m_tokens_gen)(const string &);
+        map<string, token> m_symbol_table;
+        token m_cur_tok;
+        size_t m_cur_idx;
+        vector<token> m_tokens;
+        token m_orig_symbol;
         
-    };
-    /**
-       带引用管理tdparser对象
-     */
-    class tdparser {
-        tdparser_imp * ob;
-        int       * cnt;
-        void dec_internal() {
-            if (--(*cnt) < 1) {
-                //std::cout<<"die"<<ob<<ob->to_string()<<std::endl;
-                //std::cout<<ob->string_val<<std::endl;
-                delete cnt;
-                delete ob;
-            }
+        scope m_cur_scope;
+        scope new_scope();
+        
+        token advance(string id);
+        token expression(int rbp);
+        token statement();
+        token statements();
+        token block();
+        token symbol(string id, int bp);
+        token constant(string s, string s_val, double d_val);
+        
+        string to_string() {
+            stringstream ss;
+            ss<<this<<std::endl;
+            return ss.str();
         }
-        void inc_internal() {
-            *cnt += 1;
-        }
-    public:
-        tdparser(const tdparser &other) {
-            ob = other.ob;
-            cnt = other.cnt;
-            inc_internal();
-        }
-        tdparser() {
-            ob  = new tdparser_imp;
-            cnt = new int(1);
-        }
-        tdparser_imp * operator->() {
-            return ob;
-        }
-        tdparser& operator=(const tdparser &other) {
-            dec_internal();
-            ob = other.ob;
-            cnt = other.cnt;
-            inc_internal();
-            return *this;
-        }
-        ~tdparser() {
-            dec_internal();
+        token do_parse(const string &str) {
+            m_tokens = m_tokens_gen(str);
+            m_cur_idx = 0;
+            new_scope();
+            advance("");
+            token s = statements();
+            advance("(end)");
+            s_cur_scope->pop();
+            return s;
         }
     };
 
     /**
        生成一个token
      */
-    token make_token(token_type, const string &string_val, double double_val)
+    token make_token(token_type tt, const string &string_val, double double_val)
     {
         token tok;
+        tok->type       = tt;
         tok->string_val = string_val;
         tok->double_val = double_val;
         return tok;
     }
 
+    token copy_token(token &o)
+    {
+        token tok;
+        //*tok.it() = *o.it();
+        tok->type       = o->type;
+        tok->string_val = o->string_val;
+        tok->double_val = o->double_val;
+        tok->int_val    = o->int_val;
+        tok->id         = o->id;
+        tok->reserved   = o->reserved;
+        tok->isassign   = o->isassign;
+        tok->nud        = o->nud;
+        tok->led        = o->led;
+        tok->std        = o->std;
+        tok->lbp        = o->lbp;
+        tok->rbp        = o->rbp;
+        tok->arity      = o->arity;
+        tok->at_scope   = o->at_scope;
+        tok->pos_from   = o->pos_from;
+        tok->pos_to     = o->pos_to;
+        tok->subs       = o->subs;
+//        tok->string_val = "1";
+        return tok;
+    }
+    
     char charat(const string &str, size_t i) {
         if (i < str.size())
             return str[i];
@@ -351,26 +443,284 @@ namespace epy{
         return whole;
     }
 
+    token tok_undef(token tok, tdparser_imp *prsr) {
+        tok->error("Undefined.");
+        return token();
+    }
+    token tok_miss(token tok, token left, tdparser_imp *prsr) {
+        tok->error("Missing operator.");
+        return token();
+    }
+    token tok_itself(token tok, tdparser_imp *prsr) {
+        return tok;
+    }
+    token tok_constant(token tok, tdparser_imp *prsr) {
+        s_cur_scope->reserve(tok);
+        tok->string_val = symbol_table[tok->id]->string_val;
+        tok->double_val = symbol_table[tok->id]->double_val;
+        tok->arity = ARITY_LITERAL;
+        return tok;
+    }
+    
+
+    token scope_imp::define(token tok)
+    {
+        if (m_def.count(tok->string_val)) {
+            tok->error(m_def[tok->string_val]->reserved ? "Already reserved." : "Already defined.");
+        }
+        m_def[tok->string_val] = tok;
+        tok->reserved = false;
+        tok->nud      = tok_itself;
+        tok->led      = NULL;
+        tok->std      = NULL;
+        tok->lbp      = 0;
+        tok->at_scope = s_cur_scope;
+        return tok;
+    }
+
+    token scope_imp::find(string name)
+    {
+        scope_imp *e = this; token o;
+        for(;;) {
+            if (e->m_def.count(name)) {
+                return e->m_def[name];
+            }
+            e = e->parent.it();
+            if (!e) {
+                return symbol_table[ symbol_table.count(name) ?
+                                     name : "(name)"];
+            }
+        }
+    }
+    
+    void scope_imp::pop()
+    {
+            s_cur_scope = parent;
+    }
+    
+    void scope_imp::reserve(token tok)
+    {
+        if (tok->arity != ARITY_NAME || tok->reserved) {
+            return;
+        }
+        
+        if (m_def.count(tok->string_val)) {
+            token t = m_def[tok->string_val];
+            if (t->reserved) {
+                return;
+            }
+            if (t->arity == ARITY_NAME) {
+                tok->error("Already defined.");
+            }
+        }
+        m_def[tok->string_val] = tok;
+        tok->reserved = true;
+    }
+
+    scope tdparser_imp::new_scope()
+    {
+        scope s = s_cur_scope;
+        scope ns;
+        ns->parent = s;
+        s_cur_scope = ns;
+        return ns;
+    }
+
+    token tdparser_imp::advance(string id)
+    {
+        if (!id.empty() && m_cur_tok->id != id) {
+            m_cur_tok->error(string("Expected") + id + ".");
+        }
+        if (m_cur_idx >= m_tokens.size()) {
+            m_cur_tok = symbol_table["(end)"];
+            return m_cur_tok;
+        }
+        token t = m_tokens[m_cur_idx++];
+        t->nud = NULL;
+        t->led = NULL;
+        t->std = NULL;
+        token o;
+        arity_type at = t->arity; //TODO grok from t->type
+        string sv = t->string_val;
+        token_type tv = t->type;
+        if (tv == NAME) {
+            o = m_cur_scope->find(sv);
+        } else if (tv == OPERATOR) {
+            if (!symbol_table.count(sv)) {
+                t->error("Unknown operator.");
+            }
+            o = symbol_table[sv];
+        } else if (tv == STRING || tv == NUMBER) {
+            o  = symbol_table["(literal)"];
+            at = ARITY_LITERAL;
+        } else {
+            t->error("Unknown token.");
+        }
+        token oo = copy_token(o);
+        oo->pos_from = t->pos_from;
+        oo->pos_to   = t->pos_to;
+        oo->string_val = sv;
+        oo->double_val = t->double_val;
+        oo->arity      = at;
+        return o;
+    }
+
+    token tdparser_imp::expression(int rbp)
+    {
+        token left;
+        token t = m_cur_tok;
+        advance("");
+        left = t->nud(t, this);
+        while (rbp < m_cur_tok->lbp) {
+            t = m_cur_tok;
+            advance("");
+            left = t->led(t, left, this);
+        }
+        return left;
+    }
+
+    token tdparser_imp::statement()
+    {
+        token n = m_cur_tok;
+        token v;
+        if (n->std) {
+            advance("");
+            m_cur_scope->reserve(n);
+            return n->std(n, this);
+        }
+        v = expression(0);
+        if (!v->isassign && v->id != "(") {
+            v->error("Bad expression statement.");
+        }
+        advance(";");
+        return v;
+    }
+
+    token tdparser_imp::statements()
+    {
+        vector<token> a;
+        token s;
+
+        for(;;) {
+            if (m_cur_tok->id == "}" || m_cur_tok->id == "(end)") {
+                break;
+            }
+            s = statement();
+            if (s->id != "") {
+                a.push_back(s);
+            }
+        }
+        token ta;
+        ta->subs = a;
+        return a.empty() ? token() : a.size() == 1 ? a[0] : ta;
+    }
+
+    token tdparser_imp::block()
+    {
+        token t = m_cur_tok;
+        advance("{");
+        return t->std(t, this);        
+    }
+
+    token tdparser_imp::symbol(string id, int bp)
+    {
+        token s;
+        if (symbol_table.count(id)) {
+            s = symbol_table[id];
+            if (bp >= s->lbp)
+                s->lbp = bp;
+        } else {
+            s = copy_token(m_orig_symbol);
+            s->id = s->string_val = id;
+            s->lbp = bp;
+            symbol_table[id] = s;
+        }
+        return s;
+    }
+
+    token tdparser_imp::constant(string s, string s_val, double d_val)
+    {
+        token x = symbol(s, 0);
+        x->nud = tok_constant;
+        x->string_val = s_val;
+        x->double_val = d_val;
+        return x;
+    }
+
     /**
        生成一个解析器对象
     */
     tdparser get_td_parser(vector<token> (*tokens_gen)(const string &)) {
         tdparser prsr;
+        prsr->m_tokens_gen = tokens_gen;
+        prsr->m_orig_symbol->nud = tok_undef;
+        prsr->m_orig_symbol->led = tok_miss;
+        prsr->m_orig_symbol->std = NULL;
+        prsr->symbol("(end)", 0);
+        prsr->symbol("(name)", 0);
+        prsr->symbol(":", 0);
+        prsr->symbol(";", 0);
+        prsr->symbol(")", 0);
+        prsr->symbol("]", 0);
+        prsr->symbol("}", 0);
+        prsr->symbol(",", 0);
+        prsr->symbol("else", 0);
+        return prsr;
     }
 }
 
+struct nn{
+    nn(int (*nn_)()) {
+        m_nn = nn_;
+    }
+    int operator()() {
+        return do_call();
+    }
+    virtual int do_call(){
+        if (m_nn)
+            return m_nn();
+        return 0;
+    }
+    int (*m_nn)();
+};
+
+struct nnt:nn{
+    nnt(int init): nn(NULL) {
+        m_init = init;
+    }
+    virtual int do_call(){
+        return m_init + 1;
+    }
+    int m_init;
+};
+
+int one(){return 1;};
 
 #include<sstream>
 int main(int argc, char **argv)
 {
     using namespace std;
+
+    nn *  pn1 = new nn(one);
+    vector<nn*> nns;
+    for(int i=0; i<20; ++i) {
+        nns.push_back(new nnt(i));
+    }
+
+    for(int i=0; i<20; ++i) {
+        cout<<(*nns[i])()<<endl;
+    }
+
+    cout<<(*pn1)()<<endl;
+    
     vector<epy::token> toks;
     string file = "tokens.js";
     if (argc > 2)
         file = argv[1];
-    toks = epy::get_tokens(epy::whole_file(file));
-    cout<<toks.size()<<" tokens in total"<<endl;
-
+//     toks = epy::get_tokens(epy::whole_file(file));
+//     cout<<toks.size()<<" tokens in total"<<endl;
+    epy::tdparser prsr = get_td_parser(epy::get_tokens);
+    prsr->do_parse(epy::whole_file(file));
 
     return 0;
 }
---
+++ prec.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<string>
+#include<vector>
+#include<iostream>
+#include<sstream>
+#include<fstream>
+#include<cctype>
+#include<map>
